150b5127e3c9d44ba26f1543a1eb65cb38d2c85c
martín etcheberry <msetcheberry@gmail.com>
2022-10-16 19:43:14 -0300

    View model funcionando

--- app/src/main/java/com/ort/listapp/ui/productos/ProductosFragment.kt
+++ app/src/main/java/com/ort/listapp/ui/productos/ProductosFragment.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ort.listapp.R
@@ -42,63 +44,41 @@ class ProductosFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        /*v = inflater.inflate(R.layout.fragment_productos, container, false)
-        recProdsFavoritos = v.findViewById(R.id.recViewProductos)
-        recProdsPersonalizados = v.findViewById(R.id.listaProdsPersonalizados)
-        recStock = v.findViewById(R.id.recListaStock)
-        btnFiltrado = v.findViewById(R.id.btnFiltrarProductos)
-        cargarStock()*/
-        /*cargarProdsFav()*/
-        /*cargarProdsPersonalizados()*/
-        /*return v*/
         val productosViewModel = ViewModelProvider(this).get(ProductosViewModel::class.java)
 
         _binding = FragmentProductosBinding.inflate(inflater, container, false)
         val root: View =binding.root
 
-        val recViewProductos: RecyclerView = binding.recViewProductos
-        productosViewModel.recProdsFavoritos.observe(viewLifecycleOwner){
+        val recViewProductosFav: RecyclerView = binding.recViewProductos
+        val recViewProdPersonalizados: RecyclerView = binding.listaProdsPersonalizados
+        val recViewStock: RecyclerView = binding.recListaStock
 
-            recProdsFavoritos.adapter = ProductoAdapter(productosViewModel.recProdsFavoritos,requireContext()){ prod->
+        recViewProductosFav.setHasFixedSize(true)
+        recViewProductosFav.layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
+        productosViewModel.recProdsFavoritos.observe(viewLifecycleOwner, Observer{ data->
+            recViewProductosFav.adapter = ProductoAdapter(data,requireContext()){ prod->
                 onItemClick(prod)
             }
+        })
 
-        }
-
-        return root
-    }
-
-    /*override fun onStart() {
-        super.onStart()
-
-        recProdsFavoritos.setHasFixedSize(true)
-        recProdsFavoritos.layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
-       /* viewModel.cargarProdsFavs().observe(viewLifecycleOwner, Observer{productosFavs->
-            recProdsFavoritos.adapter = ProductoAdapter(productosFavs as MutableList<Producto>,requireContext()){ pos->
-                onItemClick(pos)
+        recViewProdPersonalizados.setHasFixedSize(true)
+        recViewProdPersonalizados.layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
+        productosViewModel.recProdsPers.observe(viewLifecycleOwner, Observer{ data->
+            recViewProdPersonalizados.adapter = ProductoAdapter(data,requireContext()){ prod->
+                onItemClick(prod)
             }
+        })
 
-        })*/
-        recProdsPersonalizados.setHasFixedSize(true)
-        recProdsPersonalizados.layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
-        recStock.setHasFixedSize(true)
-        recStock.layoutManager = GridLayoutManager(requireContext(),3)
-
-        recProdsFavoritos.adapter = ProductoAdapter(listaProdsFavs,requireContext()){ prod->
-            onItemClick(prod)
-        }
-        recProdsPersonalizados.adapter =  ProductoAdapter(listaProdsPersonalizados,requireContext()){ prod->
-            onItemClick(prod)
-        }
-        recStock.adapter = ProductoAdapter(listaStock,requireContext()) { prod->
-            onItemClick(prod)
+        recViewStock.setHasFixedSize(true)
+        recViewStock.layoutManager = GridLayoutManager(requireContext(),3)
+        productosViewModel.recStock.observe(viewLifecycleOwner, Observer{ data->
+            recViewStock.adapter = ProductoAdapter(data,requireContext()){ prod->
+                onItemClick(prod)
+            }
+        })
+        return root
         }
 
-        btnFiltrado.setOnClickListener{
-
-            recProdsFavoritos.layoutManager = GridLayoutManager(requireContext(),3)
-        }
-    }*/
 
     fun onItemClick ( producto: Producto)  {
         popupBuilder = AlertDialog.Builder(context)
@@ -110,10 +90,6 @@ class ProductosFragment : Fragment() {
         var nombreProd =  popUpView.findViewById<TextView>(R.id.txt_nom_prod_popup)
         var precioProducto =  popUpView.findViewById<TextView>(R.id.txt_precio_prod_popup)
         var cantActual = 0
-
-
-       //cantidad.setText(0)
-
 
         popupBuilder.setView(popUpView)
         popUp = popupBuilder.create()
@@ -133,45 +109,6 @@ class ProductosFragment : Fragment() {
         }
     }
 
-    /*fun cargarStock(){
-        var i = 0
-        while(i<15){
-            listaStock.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-            i += 1
-        }
-    }
-    fun cargarProdsFav(){
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-    }
-    fun cargarProdsPersonalizados(){
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg" ))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg") )
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-    }
-
-    fun crearPopUp(){
-
-        popupBuilder = AlertDialog.Builder(context)
-        var popUpView = getLayoutInflater().inflate(R.layout.popup_producto_layout,null)
-        var botonAgregar = popUpView.findViewById<Button>(R.id.btn_sumar_agregarprod)
-        var botonRestar = popUpView.findViewById<Button>(R.id.btn_restar_agregarprod)
-        var cantidad = popUpView.findViewById<EditText>(R.id.txt_precio_prod_popup)
-
-        cantidad.inputType = 0
-
-
-        popupBuilder.setView(popUpView)
-        popUp = popupBuilder.create()
-        popUp.show()
-
-        }
-*/
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
