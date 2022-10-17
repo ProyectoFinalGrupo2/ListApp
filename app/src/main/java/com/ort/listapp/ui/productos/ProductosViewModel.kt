d1ff2d0b84cda94289cccec9d2dc6a9c3bd1b589
martín etcheberry <msetcheberry@gmail.com>
2022-10-17 20:59:14 -0300

    Viewmodel conectado con firebase

--- app/src/main/java/com/ort/listapp/ui/productos/ProductosViewModel.kt
+++ app/src/main/java/com/ort/listapp/ui/productos/ProductosViewModel.kt
@@ -23,8 +23,8 @@ class ProductosViewModel : ViewModel() {
             "7790250047162",
             "0080432400432",
             "7790895007057",
+            "0000077900319",
             "7790742656018",
-            "0000075024956",
             "7891000244111",
             "0000075024956",
             "4058075498051",
@@ -32,47 +32,30 @@ class ProductosViewModel : ViewModel() {
         viewModelScope.launch {
             val result: MutableList<Producto> = repositorio.getProductosByListaIds(listaProductosId)
             if(result != null){
-                result.forEach(){
-                    listaProdsPersonalizados.add(it)
-                }
-                Log.d("FUNCIONA","")
-            }else{
-                Log.d("NO FUNCIONA","")
-
+                recProdsPers.postValue(result)
             }
-        }/*
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsPersonalizados.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-    */}
+        }
+    }
 
     fun cargarProdsFav(){
         viewModelScope.launch {
             val listaProductosId = listOf(
+                "7790250047162",
                 "5410171921991",
                 "0040000017318",
-                "7790250047162",
-                "0080432400432",
-                "7790895007057",
-                "7790742656018",
-                "0000075024956",
                 "7891000244111",
                 "0000075024956",
+                "0080432400432",
                 "4058075498051",
+                "7790895007057",
+                "7790742656018",
             )
-            listaProdsFavs = repositorio.getProductosByListaIds(listaProductosId)
-
-
-            /* listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-        listaProdsFavs.add(Producto("5410171921991", "01", "0108", "MC CAIN", "Croquetas de Papas Noisettes Mc Cain 1 Kg", 978.0, 997.0, "1.0 kg"))
-    */
+            viewModelScope.launch {
+                val result: MutableList<Producto> = repositorio.getProductosByListaIds(listaProductosId)
+                if(result != null){
+                    recProdsFavoritos.postValue(result)
+                }
+            }
         }
     }
 
@@ -104,13 +87,11 @@ class ProductosViewModel : ViewModel() {
 
     val recProdsFavoritos: MutableLiveData<MutableList<Producto>> = MutableLiveData<MutableList<Producto>>().apply {
         cargarProdsFav()
-        value = listaProdsFavs
     }
 
 
     val recProdsPers: MutableLiveData<MutableList<Producto>> =  MutableLiveData<MutableList<Producto>>().apply {
         cargarProdsPersonalizados()
-        value = listaProdsPersonalizados
     }
 
 
