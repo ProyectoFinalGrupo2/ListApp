cb806ac77fc2a41a9059432de00dd71701a4235b
Valen Indovino <88804889+valenIndovino@users.noreply.github.com>
2022-10-26 21:40:36 -0300

    cambios en nombre de layout

--- app/src/main/java/com/ort/listapp/adapters/ProductoListadoAdapter.kt
+++ app/src/main/java/com/ort/listapp/adapters/ProductoListadoAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.ort.listapp.R
 import com.ort.listapp.data.ProductoRepository
 import com.ort.listapp.domain.model.Producto
@@ -24,14 +26,17 @@ class ProductoListadoAdapter(
     class ProductoListadoHolder(v: View) : RecyclerView.ViewHolder(v) {
         //Se escriben funciones que quiero que se ejecuten cuando se renderice cada item
         private var view: View
+        public val btnDelete: Button
 
         init {
             this.view = v
+            btnDelete = view.findViewById(R.id.delete)
         }
 
         fun setNombre(nombre: String) {
             var txtNombre: TextView = view.findViewById(R.id.nombre)
-            txtNombre.text = nombre
+            val nomProd = nombre.split(' ')
+            txtNombre.text = nomProd[0] + " " + nomProd[1] + "..."
         }
 
         fun setPrecioMax(precioMax: Double) {
@@ -71,13 +76,16 @@ class ProductoListadoAdapter(
 
     override fun onBindViewHolder(holder: ProductoListadoHolder, position: Int) {
         p = prods.find { it.id == prods[position].id }
-        holder.setNombre(p!!.nombre)
-        holder.setPrecioMax(p!!.precioMax)
+        p?.let { holder.setNombre(it.nombre) }
+        p?.let { holder.setPrecioMax(it.precioMax) }
         holder.setCantidad(productos[position].cantidad)
         holder.setUsuario(productos[position].usuarioId)
 
-        holder.getButton().setOnClickListener {
+        holder.btnDelete.setOnClickListener {
             onClick(productos[position])
+            Snackbar.make(
+                it,"Se elimino " + (p?.nombre ?: String), Snackbar.LENGTH_SHORT).show()
+
         }
 
     }
