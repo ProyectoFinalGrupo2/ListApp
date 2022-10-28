dc56c1a959eb6881473ac71ec08136bd2fb870cd
Rafael Dávila Pico <59027415+RADPi@users.noreply.github.com>
2022-10-28 16:40:23 -0300

    ProductosPersonalizados en Listas

--- app/src/main/java/com/ort/listapp/ui/FamilyViewModel.kt
+++ app/src/main/java/com/ort/listapp/ui/FamilyViewModel.kt
@@ -9,12 +9,14 @@ import com.ort.listapp.data.FamiliaRepository
 import com.ort.listapp.data.ProductoRepository
 import com.ort.listapp.domain.model.*
 import com.ort.listapp.helpers.SysConstants
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class FamilyViewModel : ViewModel() {
 
-    val repoProductos = ProductoRepository()
-    val repoFamilia = FamiliaRepository()
+    private val repoProductos = ProductoRepository()
+    private val repoFamilia = FamiliaRepository()
 
     private val familia by lazy {
         MutableLiveData<Familia>().also {
@@ -44,9 +46,19 @@ class FamilyViewModel : ViewModel() {
         }?.get(0)?.productos ?: emptyList()
     }
 
-    fun getProductosFavoritos(): List<Producto> {
-        val listaIds = this.familia.value?.productosFavoritos!!
-        return repoProductos.getProductosByListaIds(listaIds)
+    fun getProductosFavoritos(): List<Producto> = runBlocking(Dispatchers.IO) {
+        val prodsFav = familia.value?.productosFavoritos ?: emptyList()
+        val prodsPer = familia.value?.productosPersonalizados ?: emptyList()
+        prodsFav.map { productoId ->
+            if (productoId.startsWith(SysConstants.PREFIJO_PROD_PERS)) {
+                prodsPer.find { it.id == productoId } ?: Producto(
+                    id = productoId,
+                    nombre = "No encontrado"
+                )
+            } else {
+                repoProductos.getProductoById(productoId)
+            }
+        }
     }
 
     fun agregarProductoFavorito(idProducto: String) {
@@ -89,15 +101,15 @@ class FamilyViewModel : ViewModel() {
     }
 
     fun esProductoFav(idProducto: String): Boolean {
-        var existe = false
-        val familia = this.familia.value
-        if (familia != null) {
-            val prod = familia?.productosFavoritos?.find { it == idProducto }
-            if (prod != null) {
-                existe = true
-            }
-        }
-        return existe
+//        var existe = false
+//        val familia = this.familia.value
+//        if (familia != null) {
+//            val prod = familia?.productosFavoritos?.find { it == idProducto }
+//            if (prod != null) {
+//                existe = true
+//            }
+//        }
+        return this.familia.value?.productosFavoritos?.find { it == idProducto } != null
     }
 
     fun getProductosPersonalizados(): MutableList<Producto> {
@@ -113,9 +125,10 @@ class FamilyViewModel : ViewModel() {
                 precioMin = precio,
                 precioMax = precio,
             )
-        val familia = this.familia.value
-        familia?.productosPersonalizados?.add(producto)
-        actualizarFamilia(familia!!)
+        this.familia.value?.let { familia ->
+            familia.productosPersonalizados.add(producto)
+            actualizarFamilia(familia)
+        }
         return producto.id
     }
 
@@ -145,7 +158,7 @@ class FamilyViewModel : ViewModel() {
     ) {
         this.familia.value?.let { familia ->
             getListaByTipoEnFamilia(familia, tipoLista).removerProductoPorId(idProducto)
-            actualizarFamilia(familia!!)
+            actualizarFamilia(familia)
         }
     }
 
